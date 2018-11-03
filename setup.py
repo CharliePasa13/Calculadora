2650b5baafec21f9ff45e494a1f18348dc69c920
CharliePasa13 <clopezchacon@gmail.com>
2018-11-02 23:57:56 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Calculadora',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Charles',
+    author_email='',
+    description=''
+)
